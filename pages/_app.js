e8d4556459d1a6025f79a00d1cfcdd935ea1716f
GradadeAnimacionRCP <gradadeanimacionrcp@gmail.com>
2026-08-30 19:11:10 +0200

    Refactor MyApp component to App and add useEffect

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
+import { activarBloqueoImagenes } from '../lib/bloqueoImagenes';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const limpiar = activarBloqueoImagenes();
+    return limpiar;
+  }, []);
+
   return <Component {...pageProps} />;
 }
